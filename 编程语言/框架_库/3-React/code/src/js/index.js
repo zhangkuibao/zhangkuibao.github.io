3b564419fca420f137dda86003ca7e66e5d6ea3d
zhangkb <zkbcrow@163.com>
2020-12-03 23:29:23 +0800

    2020-12-3

--- 编程语言/框架_库/3-React/code/src/js/index.js
+++ 编程语言/框架_库/3-React/code/src/js/index.js
@@ -1,11 +1,59 @@
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchVal: 'ss',
+    }
+    this.changeSearchVal = this.changeSearchVal.bind(this);
   }
-  
-  // 调用
-const element = <Welcome name="sara" />;
+
+  changeSearchVal(val) {
+    this.setState({
+      searchVal: val,
+    })
+    console.log(2,this)
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Search searchVal={this.state.searchVal}  changeSearchVal={this.changeSearchVal}/>
+        {this.state.searchVal}
+      </div>
+    )
+  }
+}
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+    this.state = {
+      searchVal: props.searchVal,
+    }
+    this.changeSearchVal = this.changeSearchVal.bind(this);
+  }
+  changeSearchVal(e) {
+    this.props.changeSearchVal(e.target.value);
+    console.log(this)
+    // this.setState({
+    //   val: e.target.value
+    // })
+  }
+  render() {
+    return (
+      <div>
+        <input value={this.props.searchVal} onChange={this.changeSearchVal}></input>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-    element,
-    document.getElementById('root')
+  <Product />,
+  document.getElementById('root')
 );
+
+
