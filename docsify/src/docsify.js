669984e86d16ecdd92fb5cd6dc648cc9c369fdfb
zhangkuibao <zhangkuibao@haoma.com>
2021-08-17 11:23:41 +0800

    保证全局只有一个 $docsify 实例

--- docsify/src/docsify.js
+++ docsify/src/docsify.js
@@ -1,68 +1,88 @@
 const DocName = "张魁堡的笔记";
 
-export const docsify = {
-  name: DocName,
-  // nameLink: '',
-  // repo: "http://www.baidu.com",
-  // loadSidebar: "docsify/_sidebar.md",
-  loadSidebar: true,
-  // autoHeader: true,
-  loadNavbar: "docsify/assets/_navbar.md",
-  alias: {
-    ".*/_navbar.md": "docsify/assets/_navbar.md",
-    "/_sidebar.md": "技术笔记/编程基础/_sidebar.md",
-    [encodeURI("/技术笔记/编程基础/.*/_sidebar.md")]: "技术笔记/编程基础/_sidebar.md",
-    [encodeURI("/技术笔记/底层知识/.*/_sidebar.md")]: "技术笔记/底层知识/_sidebar.md",
-    [encodeURI("/技术笔记/解决方案/.*/_sidebar.md")]: "技术笔记/解决方案/_sidebar.md",
-    [encodeURI("/技术笔记/开发工具/.*/_sidebar.md")]: "技术笔记/开发工具/_sidebar.md",
-    [encodeURI("/技术笔记/代码库/.*/_sidebar.md")]: "技术笔记/代码库/_sidebar.md",
-    [encodeURI("/阅读笔记/电子栏目/.*/_sidebar.md")]: "阅读笔记/电子栏目/_sidebar.md",
-    [encodeURI("/阅读笔记/读书/.*/_sidebar.md")]: "阅读笔记/读书/_sidebar.md",
-    [encodeURI("/阅读笔记/好句/.*/_sidebar.md")]: "阅读笔记/好句/_sidebar.md",
-    [encodeURI("/阅读笔记/知识/.*/_sidebar.md")]: "阅读笔记/知识/_sidebar.md",
-  },
-  // coverpage: "docsify/_coverpage.md",
-  auto2top: true,
-  // mergeNavbar: true,
-  formatUpdated: "{YYYY}-{MM}-{DD} {HH}:{mm}",
-  // routerMode: 'history',
-  notFoundPage: "docsify/assets/_404.md",
-  topMargin: 70,
-  // homepage: '技术笔记/编程基础/编程语言/CSS/CSS手册.md',
-  // crossOriginLinks: [],  // 设置跨域链接
-  // formatUpdated: function (time) {
-  //   console.log(time)
-  //   return time;
-  // },
-  // homepage: '',
-  // logo: '',
-  // subMaxLevel: 2, // 在侧边栏中生成每个页面的目录
-  // sidebarDisplayLevel: 1, // 侧边栏默认展开级别
-  search: {
-    placeholder: "搜索",
-    noData: "找不到结果",
-  },
-  copyCode: {
-    buttonText: "复制",
-    errorText: "复制失败",
-    successText: "复制成功",
-  },
-  customPageTitle: {
-    prefix: DocName,
-    seprator: "|",
-  },
-  toc: {
-    tocMaxLevel: 6,
-    target: "H2, H3, H4, h5, h6",
-  },
-  progress: {
-    position: "top",
-    color: "var(--theme-color,#42b983)",
-    height: "3px",
-  },
-  // vueComponents,
-  // plugins: [TitleNumberPlugin.install, SidebarCollapsePlugin.install],
-};
+export let docsify = window.$docsify
+  ? window.$docsify
+  : {
+      name: DocName,
+      // nameLink: '',
+      // repo: "http://www.baidu.com",
+      // loadSidebar: "docsify/_sidebar.md",
+      loadSidebar: true,
+      // autoHeader: true,
+      loadNavbar: "docsify/assets/_navbar.md",
+      alias: {
+        ".*/_navbar.md": "docsify/assets/_navbar.md",
+        "/_sidebar.md": "技术笔记/编程基础/_sidebar.md",
+        [encodeURI(
+          "/技术笔记/编程基础/.*/_sidebar.md"
+        )]: "技术笔记/编程基础/_sidebar.md",
+        [encodeURI(
+          "/技术笔记/底层知识/.*/_sidebar.md"
+        )]: "技术笔记/底层知识/_sidebar.md",
+        [encodeURI(
+          "/技术笔记/解决方案/.*/_sidebar.md"
+        )]: "技术笔记/解决方案/_sidebar.md",
+        [encodeURI(
+          "/技术笔记/开发工具/.*/_sidebar.md"
+        )]: "技术笔记/开发工具/_sidebar.md",
+        [encodeURI(
+          "/技术笔记/代码库/.*/_sidebar.md"
+        )]: "技术笔记/代码库/_sidebar.md",
+        [encodeURI(
+          "/阅读笔记/电子栏目/.*/_sidebar.md"
+        )]: "阅读笔记/电子栏目/_sidebar.md",
+        [encodeURI(
+          "/阅读笔记/读书/.*/_sidebar.md"
+        )]: "阅读笔记/读书/_sidebar.md",
+        [encodeURI(
+          "/阅读笔记/好句/.*/_sidebar.md"
+        )]: "阅读笔记/好句/_sidebar.md",
+        [encodeURI(
+          "/阅读笔记/知识/.*/_sidebar.md"
+        )]: "阅读笔记/知识/_sidebar.md",
+      },
+      // coverpage: "docsify/_coverpage.md",
+      auto2top: true,
+      // mergeNavbar: true,
+      formatUpdated: "{YYYY}-{MM}-{DD} {HH}:{mm}",
+      // routerMode: 'history',
+      notFoundPage: "docsify/assets/_404.md",
+      topMargin: 70,
+      // homepage: '技术笔记/编程基础/编程语言/CSS/CSS手册.md',
+      // crossOriginLinks: [],  // 设置跨域链接
+      // formatUpdated: function (time) {
+      //   console.log(time)
+      //   return time;
+      // },
+      // homepage: '',
+      // logo: '',
+      // subMaxLevel: 2, // 在侧边栏中生成每个页面的目录
+      // sidebarDisplayLevel: 1, // 侧边栏默认展开级别
+      search: {
+        placeholder: "搜索",
+        noData: "找不到结果",
+      },
+      copyCode: {
+        buttonText: "复制",
+        errorText: "复制失败",
+        successText: "复制成功",
+      },
+      customPageTitle: {
+        prefix: DocName,
+        seprator: "|",
+      },
+      toc: {
+        tocMaxLevel: 6,
+        target: "H2, H3, H4, h5, h6",
+      },
+      progress: {
+        position: "top",
+        color: "var(--theme-color,#42b983)",
+        height: "3px",
+      },
+      // vueComponents,
+      // plugins: [TitleNumberPlugin.install, SidebarCollapsePlugin.install],
+    };
 
 export function docsifyInstallPlugin(plugin) {
   docsify.plugins
