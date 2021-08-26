4181df2642e7c28bf30aa588fde47a2707624b88
zhangkuibao <zhangkuibao@haoma.com>
2021-08-26 10:51:36 +0800

    h标签序号插件调整

--- docsify/src/plugins/TitleNumberPlugin.js
+++ docsify/src/plugins/TitleNumberPlugin.js
@@ -2,9 +2,9 @@ export default class TitleNumberPlugin {
   titleArr = [];
 
   static transferTitleNumber(title, text) {
-    let arr = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十"];
-    if (title.length === 1) {
-      return arr[title[0]] + "、" + text;
+    let titleList = title.split('.')
+    if (titleList.length === 1) {
+      return TitleNumberPlugin.intToChinese(titleList[0]) + "、" + text;
     }
     return title + " " + text;
   }
@@ -44,6 +44,39 @@ export default class TitleNumberPlugin {
       }
     });
   }
+  
+  static intToChinese ( str ) {
+    str = str+'';
+    var len = str.length-1;
+    var idxs = ['','十','百','千','万','十','百','千','亿','十','百','千','万','十','百','千','亿'];
+    // var num = ['零','壹','贰','叁','肆','伍','陆','柒','捌','玖'];
+    var num = ["", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十"];
+    
+    return str.replace(/([1-9]|0+)/g,function( $, $1, idx, full) {
+     var pos = 0;
+     if( $1[0] != '0' ){
+      pos = len-idx;
+      if( idx == 0 && $1[0] == 1 && idxs[len-idx] == '十'){
+       return idxs[len-idx];
+      }
+      return num[$1[0]] + idxs[len-idx];
+     } else {
+      var left = len - idx;
+      var right = len - idx + $1.length;
+      if( Math.floor(right/4) - Math.floor(left/4) > 0 ){
+       pos = left - left%4;
+      }
+      if( pos ){
+       return idxs[pos] + num[$1[0]];
+      } else if( idx + $1.length >= len ){
+       return '';
+      }else {
+       return num[$1[0]]
+      }
+     }
+    });
+   }
+
   static install(hook, vm) {
     //   hook.init(function () {
     //     // 初始化完成后调用，只调用一次，没有参数。
