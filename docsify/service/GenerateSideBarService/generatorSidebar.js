bb242ade7a30efdce1aef93d57b7368814e62d44
zhangkuibao <zhangkuibao@haoma.com>
2021-08-31 12:10:49 +0800

    auto-sidebar调整

--- docsify/service/GenerateSideBarService/generatorSidebar.js
+++ docsify/service/GenerateSideBarService/generatorSidebar.js
@@ -8,6 +8,7 @@ function ergodicConfig(dirMap, level = 0) {
   // 处理目录
   if (level === 2) {
     generateSidbarPart(dirMap);
+    return;
   }
   for (let prop in dirMap.childDir) {
     let item = dirMap.childDir[prop];
@@ -15,58 +16,54 @@ function ergodicConfig(dirMap, level = 0) {
   }
 }
 
-function generateSidbarPart(dirMap) {
-  let root = dirMap.dir;
+function generateSidbarPart(partDirMap) {
+  let root = partDirMap.dir;
   sidbarMdMap[root] = "";
 
-  ergodic(dirMap["childDir"]);
+  ergodic(partDirMap);
 
   function ergodic(rootMap, level = 0) {
-    for (let prop in rootMap) {
-      let item = rootMap[prop];
-
-      // 自身及深层没有文件时退出
-      if (!hasFile(item)) continue;
-
-      sidbarMdMap[root] += `${spaceStr(level)}- ${prop}\n\n`;
-
-      // 有子目录
-      if (Object.keys(item.childDir).length) {
-        ergodic(item.childDir, level + 1);
-      }
-
-      // 有文件
-      if (item.files.length) {
-        item.files.forEach((file, index) => {
-          sidbarMdMap[root] += `${spaceStr(level + 1)}- [${file.filename}](${
-            file.fullpath
-          })\n${item.files.length - 1 === index ? "\n" : ""}`;
-        });
-      }
-      generateSidbar();
-    }
-  }
-
-  function hasFile(rootMap) {
+    // 有文件
     if (rootMap?.files?.length) {
-      return true;
+      rootMap.files.forEach((file, index) => {
+        sidbarMdMap[root] += `${spaceStr(level)}- [${file.filename}](${
+          file.fullpath
+        })\n${rootMap.files.length - 1 === index ? "\n" : ""}`;
+      });
     }
     for (let prop in rootMap.childDir) {
       let item = rootMap.childDir[prop];
-      let childResult = [];
-      if (item?.files?.length) {
-        return true;
+
+      // 自身及深层没有文件时退出
+      if (hasFile(item)) {
+        sidbarMdMap[root] += `${spaceStr(level)}- ${prop}\n\n`;
+        ergodic(item, level + 1);
       }
-      Object.keys(item.childDir).forEach((prop) => {
-        childResult.push(hasFile(item.childDir[prop]));
-      });
-      // console.log(item.dir, hasFile(item.childDir))
-      if (childResult.some((ele) => ele)) {
-        return true;
-      }
+
+      generateSidbar();
     }
-    return false;
   }
+}
+
+function hasFile(rootMap) {
+  if (rootMap?.files?.length) {
+    return true;
+  }
+  for (let prop in rootMap.childDir) {
+    let item = rootMap.childDir[prop];
+    let childResult = [];
+    if (item?.files?.length) {
+      return true;
+    }
+    Object.keys(item.childDir).forEach((prop) => {
+      childResult.push(hasFile(item.childDir[prop]));
+    });
+    // console.log(item.dir, hasFile(item.childDir))
+    if (childResult.some((ele) => ele)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function generateSidbar() {
@@ -75,7 +72,11 @@ function generateSidbar() {
     Object.keys(sidbarMdMap).forEach((prop) => {
       let targetDirname = path.resolve(prop, "_sidebar.md");
       let urlList = prop.split(path.sep);
-      let topTitle = `<div class="sidebar-title">${urlList[urlList.length - 2]}</div>\n<template id="root-breadcrumb">${urlList[urlList.length - 1]}</template>\n\n`;
+      let topTitle = `<div class="sidebar-title">${
+        urlList[urlList.length - 2]
+      }</div>\n<template id="root-breadcrumb">${
+        urlList[urlList.length - 1]
+      }</template>\n\n`;
       fs.writeFile(targetDirname, topTitle + sidbarMdMap[prop], () => {
         console.log(`${targetDirname} 写入成功`);
       });
