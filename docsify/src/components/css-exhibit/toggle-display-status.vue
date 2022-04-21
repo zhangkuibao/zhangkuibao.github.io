84c0477e37d902709c9ea8f07b177efe5107ebf2
zhangkuibao <zhangkuibao@haoma.com>
2022-04-21 15:16:50 +0800

    【样式效果】用 clip-path 实现向右收起效果

--- docsify/src/components/css-exhibit/toggle-display-status.vue
+++ docsify/src/components/css-exhibit/toggle-display-status.vue
@@ -1,9 +1,15 @@
 <template>
-  <app-example title="clip-path 实现元素渐变隐藏">
-    <div class="display-box">
-      <div>将鼠标放在红色区域内</div>
-      <div>元素会向右收起</div>
-      <img style="width: 100px" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F4k%2Fs%2F02%2F2109242312005c1-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1653116103&t=d3ee38e4904ce1fdc1eb759f60163c66" alt="">
+  <app-example title="clip-path 实现向右卷起效果">
+    <div class="wrapper">
+      <div class="display-box">
+        <div>将鼠标放在红色区域内</div>
+        <div>元素会向右收起</div>
+        <img
+          style="width: 100px"
+          src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F4k%2Fs%2F02%2F2109242312005c1-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1653116103&t=d3ee38e4904ce1fdc1eb759f60163c66"
+          alt=""
+        />
+      </div>
     </div>
   </app-example>
 </template>
@@ -23,7 +29,11 @@ export default {
   clip-path: inset(0 0 0 0);
 }
 
-.display-box:hover {
+.wrapper {
+  border: 1px solid;
+}
+
+.wrapper:hover .display-box {
   clip-path: inset(0 0 0 100%);
 }
 </style>
