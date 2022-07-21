6a35aafca42f6b7eb4f43c511185931ba3d9b622
zhangkuibao <zhangkuibao@haoma.com>
2022-07-21 14:25:53 +0800

    2022-07-21_14:25:52

---
+++ js/baidu.statistic.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?7367bac87468f75dfa2fda1061b6a452";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
